887dabc740b8c0d98db20effdcfd354ff45366d1
suntinghui <tinghuisun@163.com>
2017-05-12 10:17:36 +0800

    STH

--- js/util.js
+++ js/util.js
@@ -19,13 +19,17 @@ function isPhoneNum(str) {　　
 }
 
 function getHttpErrorDesp(type) {
+//	mui.plusReady(function() {
+//		plus.nativeUI.closeWaiting(); 
+//	});
+
 	var desp = '未知异常,请重试';
 	if(type == 'timeout') {
 		desp = '网络请求超进,请重试';
 	} else if(type == 'error') {
 		desp = '请求出错,请稍候再试';
 	} else if(type == 'abort') {
-		desp = '请求被终止,请重试';
+		desp = '请求被终止,可能是网络连接问题';
 	} else if(type == 'parsererror') {
 		desp = '服务器返回数据异常,请重试';
 	} else if(type == 'null') {
@@ -34,3 +38,48 @@ function getHttpErrorDesp(type) {
 
 	return desp;
 }
+
+function checkNetwork() {
+	// 用没有网络时返回abort
+	return;
+
+	if(window.plus) {　　
+		onNetChange();
+	} else {　　
+		document.addEventListener("netchange", onNetChange, false);
+	}
+}
+
+// 如果返回真则说明有网络,否则没有网络
+function onNetChange() {
+	var nt = plus.networkinfo.getCurrentType();
+	if(nt == plus.networkinfo.CONNECTION_UNKNOW || nt == plus.networkinfo.CONNECTION_NONE) {
+		mui.toast("当前没有网络,请检查网络设置");
+		return false;
+	}
+
+	return true;
+}
+
+/**
+//获取当前网络类型
+function onNetChange() {
+	var nt = plus.networkinfo.getCurrentType();　　
+	switch(nt) {　　　　
+		case plus.networkinfo.CONNECTION_ETHERNET:
+		case plus.networkinfo.CONNECTION_WIFI:
+			mui.toast("当前网络为WiFi");
+			break;
+
+		case plus.networkinfo.CONNECTION_CELL2G:
+		case plus.networkinfo.CONNECTION_CELL3G:
+		case plus.networkinfo.CONNECTION_CELL4G:
+			mui.toast("当前网络非WiFi");
+			break;　　　　
+		default:
+			mui.toast("当前没有网络");
+			break;　　
+	}
+}
+
+**/
